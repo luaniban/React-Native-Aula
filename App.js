e9d348e65276111841ceb41154886a791249cd34
luaniban <luanibanhez@gmail.com>
2026-08-04 20:04:03 -0300

    add: implementacao de logo com image e titulo, descricao e imagem para o card

--- App.js
+++ App.js
@@ -1,24 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-
+        <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          gap: 10,
+          width: '90%',
+        }}
+        >
+        <Image
+          source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+          style={{ width: 60, height: 60, borderRadius: 30 }}
+          />
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>My App</Text>
+        
+        </View>
       </View>
        <View style={styles.section1}>
           <View style={styles.content_cards}>
             <View style={styles.card}>
-
+              <Image
+                source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+                style={{ width: 60, height: 60, borderRadius: 30 }}
+              />
+              <View style={{ flex: 1 }}>
+                <Text style={{ fontSize: 16, fontWeight: 'bold', marginTop: 10 }}>Titulo</Text>
+                <Text style={{ fontSize: 14 }}>This is the content for Card 1</Text>
+              </View>
             </View>
 
              <View style={styles.card}>
-
+              <Image
+                source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+                style={{ width: 60, height: 60, borderRadius: 30 }}
+              />
+              <View style={{ flex: 1 }}>
+                <Text style={{ fontSize: 16, fontWeight: 'bold', marginTop: 10 }}>Titulo</Text>
+                <Text style={{ fontSize: 14 }}>This is the content for Card 1</Text>
+              </View>
             </View>
-
-             <View style={styles.card}>
-
+              <View style={styles.card}>
+              <Image
+                source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+                style={{ width: 60, height: 60, borderRadius: 30 }}
+              />
+              <View style={{ flex: 1 }}>
+                <Text style={{ fontSize: 16, fontWeight: 'bold', marginTop: 10 }}>Titulo</Text>
+                <Text style={{ fontSize: 14 }}>This is the content for Card 1</Text>
+              </View>
             </View>
           </View>
       </View>
@@ -53,7 +88,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'wrap',
     margin: 20,
-    width: 350,
+    width: "70%",
     padding: 20,
     borderRadius: 20,
     backgroundColor: '#c0c3ce',
@@ -62,6 +97,10 @@ const styles = StyleSheet.create({
 
   card: {
     flex: 1,
+     flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+    padding: 10,
     maxHeight: 100,
     borderRadius: 5,
     backgroundColor: '#fff'
